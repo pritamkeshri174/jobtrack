8d638011add5d0d8e9dd7ca46bf5682287455a75
Pritam Keshri <pritamkeshri174@gmail.com>
2026-09-23 12:57:46 +0530

    Fix Job model import for Render

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Job = require("../models/Job");
+const Job = require("../models/job");
 const authMiddleware = require("../authMiddleware");
 
 const router = express.Router();
